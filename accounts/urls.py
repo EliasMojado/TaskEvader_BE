0467eb23f19de776683e2e9a65a810b2bffd55fd
Kent Dave Catipay <kpcatipay@up.edu.ph>
2025-05-03 16:50:13 +0800

    add health check and remove token-auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import MyAccountAPIView, RegisterAPIView, ProfileByIdAPIView, CompleteProfileAPIView, ChangePasswordAPIView, UserSearchAPIView
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import MyAccountAPIView, RegisterAPIView, ProfileByIdAPIView, CompleteProfileAPIView, ChangePasswordAPIView, \
+    UserSearchAPIView, HealthCheckAPIView
 
 urlpatterns = [
     path('account/', MyAccountAPIView.as_view(), name='my-account'),
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('token-auth/', obtain_auth_token, name='api_token_auth'),
     path('profile/<int:pk>/', ProfileByIdAPIView.as_view(), name='profile-by-id'),
     path('complete-profile/', CompleteProfileAPIView.as_view(), name='complete-profile'),
     path('change-password/', ChangePasswordAPIView.as_view(), name='change-password'),
     path('search-users/', UserSearchAPIView.as_view(), name='search-users'),
+    path('health/', HealthCheckAPIView.as_view(), name='health-check'),
 ]
